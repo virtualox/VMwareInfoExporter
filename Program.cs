1ae15c8f9bb9e5a84cd4bf75e7ba301b69b37775
VirtualOx <37578273+virtualox@users.noreply.github.com>
2023-10-20 10:30:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security;
+using VMware.Vim;
+
+namespace VMwareInfoExporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 || args.Contains("/?") || args.Contains("-?") || args.Contains("-help") || args.Contains("-h"))
+            {
+                DisplayHelp();
+                return;
+            }
+
+            string server = null;
+            string outputPath = null;
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i].ToLower())
+                {
+                    case "-server":
+                        if (i + 1 < args.Length)
+                        {
+                            server = args[i + 1];
+                            i++;
+                        }
+                        break;
+                    case "-outputpath":
+                        if (i + 1 < args.Length)
+                        {
+                            outputPath = args[i + 1];
+                            i++;
+                        }
+                        break;
+                }
+            }
+
+            if (string.IsNullOrEmpty(server) || string.IsNullOrEmpty(outputPath))
+            {
+                DisplayHelp();
+                return;
+            }
+
+            Console.WriteLine($"Server: {server}");
+            Console.WriteLine($"Output Path: {outputPath}");
+
+            Console.WriteLine($"Enter username for {server}:");
+            string username = Console.ReadLine();
+
+            Console.WriteLine($"Enter password for {server}:");
+            SecureString password = GetPassword();
+
+            var client = new VimClientImpl();
+            client.Connect($"https://{server}/sdk");
+            client.Login(username, ToInsecureString(password));
+
+            var vms = client.FindEntityViews(typeof(VirtualMachine), null, null, null);
+            List<VmInfo> output = new List<VmInfo>();
+
+            foreach (VirtualMachine vm in vms)
+            {
+                var guest = vm.Guest;
+                var info = new VmInfo
+                {
+                    VMName = vm.Name,
+                    IPAddress = guest.IpAddress,
+                    GuestOS = guest.GuestFullName,
+                    Notes = vm.Config.Annotation
+                };
+                output.Add(info);
+            }
+
+            WriteToCsv(outputPath, output);
+
+            client.Disconnect();
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("VMwareInfoExporter - Exports VM info from vCenter or ESXi to a CSV file.");
+            Console.WriteLine("Usage:");
+            Console.WriteLine("VMwareInfoExporter.exe -server <server_address> -outputpath <path_to_csv>");
+            Console.WriteLine("Switches:");
+            Console.WriteLine("-server     : The address of the vCenter or ESXi host.");
+            Console.WriteLine("-outputpath : The path to the CSV file where data should be saved.");
+            Console.WriteLine("-? /? -help -h: Displays this help information.");
+        }
+
+        private static SecureString GetPassword()
+        {
+            SecureString password = new SecureString();
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    password.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+                else if (key.Key == ConsoleKey.Backspace && password.Length > 0)
+                {
+                    password.RemoveAt(password.Length - 1);
+                    Console.Write("\b \b");
+                }
+            } while (key.Key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            return password;
+        }
+
+        private static string ToInsecureString(SecureString secureString)
+        {
+            IntPtr unmanagedString = IntPtr.Zero;
+            try
+            {
+                unmanagedString = System.Runtime.InteropServices.Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return System.Runtime.InteropServices.Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+        private static void WriteToCsv(string path, List<VmInfo> vms)
+        {
+            using (var sw = new StreamWriter(path))
+            {
+                sw.WriteLine("VM Name\tIP Address\tGuest OS\tNotes");
+                foreach (var vm in vms)
+                {
+                    sw.WriteLine($"{vm.VMName}\t{vm.IPAddress}\t{vm.GuestOS}\t{vm.Notes}");
+                }
+            }
+        }
+
+        public class VmInfo
+        {
+            public string VMName { get; set; }
+            public string IPAddress { get; set; }
+            public string GuestOS { get; set; }
+            public string Notes { get; set; }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VMware VM Info Exporter")]
+[assembly: AssemblyDescription("A console application to extract and export virtual machine information from VMware vCenter or ESXi hosts to a CSV file.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VirtualOx B.V.")]
+[assembly: AssemblyProduct("VMwareInfoExporter Tool")]
+[assembly: AssemblyCopyright("Copyright © 2023 VirtualOx B.V. Released under the GNU GPLv3 License.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("790fc0f1-5ca2-49c1-96c0-d323dc753a24")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
